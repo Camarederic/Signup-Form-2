162ce52f5a95e8e20e783162f363fc03cb931266
Camarederic <camarederic@gmail.com>
2026-03-12 21:25:22 +0300

    added code and more styles for the site JS & CSS

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-'use strict'
+"use strict";
 
+const container = document.querySelector(".container");
+const signUpLink = document.querySelector(".signup-link");
+const signInLink = document.querySelector(".signin-link");
+
+signUpLink.addEventListener("click", () => {
+  container.classList.add("navigate");
+});
+
+signInLink.addEventListener("click", () => {
+  container.classList.remove("navigate");
+});
